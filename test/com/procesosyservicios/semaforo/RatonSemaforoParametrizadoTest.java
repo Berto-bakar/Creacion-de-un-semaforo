c261e8fadf7a4ec32ad7c12969eeaf1a2c96aa85
Berto-bakar <alberto.samlob@educa.jcyl.es>
2024-12-02 23:42:50 +0100

    Junit automatico

---
+++ test/com/procesosyservicios/semaforo/RatonSemaforoParametrizadoTest.java
@@ -0,0 +1,71 @@
+
+package com.procesosyservicios.semaforo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class RatonSemaforoParametrizadoTest {
+    
+    
+    
+    public RatonSemaforoParametrizadoTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of comer method, of class RatonSemaforoParametrizado.
+     */
+    @Test
+    public void testComer() {
+        System.out.println("comer");
+        RatonSemaforoParametrizado instance = null;
+        instance.comer();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of run method, of class RatonSemaforoParametrizado.
+     */
+    @Test
+    public void testRun() {
+        System.out.println("run");
+        RatonSemaforoParametrizado instance = null;
+        instance.run();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class RatonSemaforoParametrizado.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        RatonSemaforoParametrizado.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
